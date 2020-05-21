a6a0ea3612f77ebb318923096d5256ad2b85e562
Ricardo Lara <alejandro.elara@live.com>
2020-05-21 12:27:47 -0500

    creates header

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+// pages
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
-const HatsPage = () => (<div>
-  <h1>Hats</h1>
+// comp
+import Header from './components/header/header.component'
+
+const ContactPage = () => (<div>
+  <h1>contact</h1>
 </div>)
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route path='/contact' component={ContactPage} />
+        {/* <Route path='/shop/:category' component={HatsPage} /> */}
       </Switch>
     </div>
   );

4ecf3a9b83ba0daad949f6b56647b76317151551
Ricardo Lara <alejandro.elara@live.com>
2020-05-24 18:27:44 -0500

    update app to redirect to home is it has current user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import './App.css';
@@ -57,8 +57,13 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
           <Route path='/contact' component={ContactPage} />
+          <Route path='/signin' render={() => 
+            this.props.currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInAndSignUpPage />
+            )} />
           {/* <Route path='/shop/:category' component={HatsPage} /> */}
         </Switch>
       </div>
@@ -66,7 +71,15 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(App);

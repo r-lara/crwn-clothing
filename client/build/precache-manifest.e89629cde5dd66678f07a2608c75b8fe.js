6e601ca9577ffb1283a06da359d580b9d2c08348
Ricardo Lara <alejandro.elara@live.com>
2020-06-16 12:58:16 -0500

    asdf

---
+++ client/build/precache-manifest.e89629cde5dd66678f07a2608c75b8fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb65320263dd29e9217e99a30e10e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e1bb25c6988869571c",
+    "url": "/static/css/main.3aeb53d6.chunk.css"
+  },
+  {
+    "revision": "bda34385c81f7d29aedc",
+    "url": "/static/js/2.c58880f8.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.c58880f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e1bb25c6988869571c",
+    "url": "/static/js/main.42a6dd2a.chunk.js"
+  },
+  {
+    "revision": "6bedc7d4e72b37a14961",
+    "url": "/static/js/runtime-main.059467c3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
